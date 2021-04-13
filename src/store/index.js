4e60ece5595ce15a7c6756b192a5ae4e09ba7649
Luismatos19 <luismatosrobot@gmail.com>
2021-04-13 12:49:07 -0300

    config

---
+++ src/store/index.js
@@ -0,0 +1,20 @@
+import createPersistedState from 'vuex-persistedstate';
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  strict: true,
+  state: {
+  },
+  mutations: {
+  },
+  actions: {
+  },
+  modules: {
+  },
+  plugins: [
+    createPersistedState(),
+  ],
+});
